db38101e892046023fd443960360bfe72c1d30aa
Vladislav Zavertaylo <102727549+vladislavzaver91@users.noreply.github.com>
2022-09-15 18:34:16 +0300

    Добавляет решение задачи 2 по JS mod-7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `<div class="gallery__item">
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        </div>`;
+    }).join('');
+};
+
+function onGalleryContainerClick(evt) {
+    evt.preventDefault();
+
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+    var lightbox = new SimpleLightbox('.gallery a', {captionDelay : 250, captionsData: 'alt'});
+};
